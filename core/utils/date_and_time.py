a14924152ea70445013d14cefc4024ec66e87579
Saeed <saeedhayati32@gmail.com>
2024-02-11 18:22:08 +0330

    improved: moved some function to utils

--- core/utils/date_and_time.py
+++ core/utils/date_and_time.py
@@ -9,3 +9,18 @@ async def get_date(day="today"):
         return f"{today.year}-{today.month}-{today.day}"
     else:
         return f"{tomorrow.year}-{tomorrow.month}-{tomorrow.day}"
+
+
+async def get_day_name(day="today"):
+    today = datetime.date.today()
+    tomorrow = today + datetime.timedelta(days=1)
+    today_name = today.strftime("%A")[:3]
+    tomorrow_name = tomorrow.strftime('%A')[:3]
+
+    if day == 'today':
+        return f"{today_name}"
+    elif day == 'tomorrow':
+        return f"{tomorrow_name}"
+
+
+
