2a24ff3c99dcf4a0fb10cd6af2c30f237b646879
Saeed <saeedhayati32@gmail.com>
2024-02-22 11:11:30 +0330

    adding new rows and buttons 17

--- test_web_app.py
+++ test_web_app.py
@@ -1,47 +1,30 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
-
-# Sample data for dynamic form generation
-fields = ['Name', 'Email', 'Phone']
-
+app.secret_key = 'your_secret_key'
 
 # Sample data for dynamic form generation
 data = {
-    "subject1": {"under_subject1-1": {"m1": 0, "m2": 0, "m3": 0}, "under_subject1-2": {"m1": 0, "m2": 0, "m3": 0}},
-    "subject2": {"under_subject2-1": {"m1": 0, "m2": 0, "m3": 0}, "under_subject2-2": {"m1": 0, "m2": 0, "m3": 0}}
+    "subject1": {
+        "under_subject1-1": ["m1", "m2", "m3"],
+        "under_subject1-2": ["n1", "n2", "n3"]
+    },
+    "subject2": {
+        "under_subject2-1": ["k1", "k2", "k3"],
+        "under_subject2-2": ["p1", "p2", "p3"]
+    }
 }
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    num_additional_fields = int(request.form.get('num_additional_fields', 0))
     if request.method == 'POST':
-        if 'add_field' in request.form:
-            # Increment the number of additional fields
-            num_additional_fields += 1
-            return render_template('dynamic_form.html', fields=fields, data=data,
-                                   num_additional_fields=num_additional_fields, form_data=request.form)
-        else:
-            # Process the form data here
-            form_data = {}
-            for field in fields:
-                form_data[field.lower()] = request.form.get(field.lower())
-            for i in range(num_additional_fields):
-                new_field_name = f'new_field_{i + 1}'
-                form_data[new_field_name] = request.form.get(new_field_name, '')
-            # Redirect to a new route to display the submitted data
-            return redirect(url_for('display_data', **form_data))
+        # Store form data in session
+        session['form_data'] = dict(request.form)
+        return redirect(url_for('index'))  # Redirect to reload the page
 
-    return render_template('dynamic_form.html', data=data,
-                           fields=fields, num_additional_fields=num_additional_fields)
-
-
-@app.route('/display_data')
-def display_data():
-    # Get the submitted form data from the URL parameters
-    form_data = request.args.to_dict()
-    return render_template('display_data.html', form_data=form_data)
+    form_data = session.get('form_data', {})
+    return render_template('dynamic_form.html', data=data, form_data=form_data)
 
 
 if __name__ == '__main__':
