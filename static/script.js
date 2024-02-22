0cb0a87aace0cb11ec6aef331fc2f197a24f5247
Saeed <saeedhayati32@gmail.com>
2024-02-22 12:42:07 +0330

    11

--- static/script.js
+++ static/script.js
@@ -1,6 +1,7 @@
 // static/script.js
 
 function subjectChanged(selectElement, subjects) {
+    alert("we are in the function")
     const underSubjectsDropdown = selectElement.parentElement.querySelector('.under-subject');
     const underUnderSubjectsDropdown = selectElement.parentElement.querySelector('.under-under-subject');
     const selectedSubject = selectElement.value;
@@ -28,7 +29,6 @@ function subjectChanged(selectElement, subjects) {
 }
 
 function underSubjectChanged(selectElement) {
-    console.log("changed")
     const underUnderSubjectsDropdown = selectElement.parentElement.querySelector('.under-under-subject');
     const selectedSubject = selectElement.parentElement.querySelector('.subject').value;
     const selectedUnderSubject = selectElement.value;
